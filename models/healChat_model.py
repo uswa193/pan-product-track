cca9c144e6a5e6bd4b6197f13262233d3c5404d6
Uswatun Nurjanah <130342016+uswa193@users.noreply.github.com>
2024-06-08 21:45:08 +0700

    Update healChat_model.py

--- models/healChat_model.py
+++ models/healChat_model.py
@@ -1,1 +1,15 @@
+import tensorflow as tf
 
+class HealChatModel:
+    def __init__(self):
+        self.model = tf.keras.models.load_model('path_to_healChat_model.h5')
+
+    def predict(self, text):
+        # Preprocessing and prediction logic
+        processed_text = self.preprocess(text)
+        prediction = self.model.predict(processed_text)
+        return prediction
+
+    def preprocess(self, text):
+        # Add preprocessing steps here
+        return text
