a04cf39d71fcbb6d209b934ff8455a95f6e79046
Uswatun Nurjanah <130342016+uswa193@users.noreply.github.com>
2024-06-11 13:54:39 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const serviceAccount = require('./serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
