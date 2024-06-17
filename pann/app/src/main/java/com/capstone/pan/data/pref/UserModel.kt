70c210043561a593af398c7b9eb7a0608efd5073
ghiyatssyahied <131997490+ghiyatssyahied@users.noreply.github.com>
2024-06-18 01:55:23 +0700

    Delete pann directory

--- pann/app/src/main/java/com/capstone/pan/data/pref/UserModel.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.picodiploma.pan.data.pref
-
-data class UserModel(
-    val email: String,
-    val token: String,
-    val isLogin: Boolean = false
-)
--- pann/app/src/main/java/com/capstone/pan/ui/ChatViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.picodiploma.pan.ui
-
-import androidx.lifecycle.ViewModel
-
-class ChatViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
